ac2a3f598ea30afcb1200d2520acae319c6ddbfd
Sumit Kumar <sumit11095@gmail.com>
2017-04-18 14:18:34 +0530

    Revert "calibration"
    This reverts commit 424395091ef72a65f6aa71bc1a7e8028216e21a9.

--- app/src/main/java/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
+++ app/src/main/java/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
@@ -1055,11 +1055,11 @@ public class ColorBlobDetectionActivity extends Activity implements OnTouchListe
                 calibrationCount = calibrationCount % calibrationFrameRate;
                 noCalibrationCount = 0;
                 if (prevCalibrationState != calibrated) {
-                    String toSpeak = "Both tags now in view";
-                    // Log.i("CalCheck", "Calibrated");
+                    calibrationCount = 0;
+                    final String toSpeak = "Both tags now in view";
+                    Log.i("CalCheck", "Calibrated");
                     // Add in queue even if tts isSpeaking(), speakOut doesn't add if isSpeaking()
                     speakOut(toSpeak, applicationContext);
-                    calibrationCount = 0;
                 }
             } else {
                 noCalibrationCount++;
